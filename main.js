5cb2a1d9858a02e5e6c990b5ddf299fcf53ba0cc
Brayden Lopez <brayden.lopez@extendhealth.com>
2016-05-06 13:39:58 -0600

    Adding as non-ending script for constant polling

--- main.js
+++ main.js
@@ -8,51 +8,48 @@ const fs = require('fs');
 const config = require("./config.json");
 const mkdirp = require('mkdirp');
 
-gmusic.init({email: config.email, password: config.password}, function(err) {
-  if(err) {
-    console.error(err);
-    return;
-  }
+var id = setInterval(() => {
+  gmusic.init({email: config.email, password: config.password}, function(err) {
+    if(err) {
+      console.error(err);
+      return;
+    }
 
-  gmusic.getPlayListEntries(function(err, data) {
+    console.log("Polling playlist");
+    gmusic.getPlayListEntries((err, data) => {
+        var songs = data.data.items.filter(entry => entry.playlistId == config.playlist_cid).sort((a, b) => a.absolutePosition - b.absolutePosition);
 
-      var songs = data.data.items.filter(entry => {
+        var lastSong = songs[songs.length - 1];
+        var supposedToBeLast = songs.findIndex(entry => entry.track && entry.track.title == config.last_song_title); // All of the lights CID //
+        var firstSong = songs[0];
 
-        // Make sure it belongs in our selected playlist
-        // Make sure its a song in the playlist
-        // Make sure its not already in the other playlist
-        return entry.playlistId == "def948ca-ce6a-4a06-920e-2f4b58f0fdd9";
-      }).sort((a, b) => a.absolutePosition - b.absolutePosition);
+        if (supposedToBeLast == -1) {
+          console.log("Cannot find last song. Was it deleted?");
+          clearInterval(id);
+          return;
+        }
 
-      var lastSong = songs[songs.length - 1];
-      var supposedToBeLast = songs.findIndex(function(entry) { return entry.track && entry.track.title == "All Of The Lights (Explicit)" }); // All of the lights CID //
-      var firstSong = songs[0];
+        if (supposedToBeLast == songs.length - 1) {
+          // nothing to update //
+          console.log("Nothing to update");
+          return;
+        }
 
-      if (supposedToBeLast == -1) {
-        console.log("Cannot find last song. Was it deleted?");
-        return;
-      }
+        var songsToMove = songs.slice(supposedToBeLast + 1);
 
-      if (supposedToBeLast == songs.length - 1) {
-        // nothing to update //
-        console.log("Nothing to update");
-        return;
-      }
+        // Take a snapshot of the playlist order //
+        console.log("Saving current playlist snapshot to: './snapshots/" + moment().format("MMM_do_YY_hh_mm_ss") + ".json'")
+        mkdirp.sync("./snapshots");
+        fs.writeFileSync('./snapshots/' + moment().format("MMM_do_YY_hh_mm_ss") + '.json', JSON.stringify(songs, null, 2), 'utf-8');
 
-      var songsToMove = songs.slice(supposedToBeLast + 1);
-
-      // Take a snapshot of the playlist order //
-      console.log("Saving current playlist snapshot to: './snapshots/" + moment().format("MMM_do_YY_hh_mm_ss") + ".json'")
-      mkdirp.sync("./snapshots");
-      fs.writeFileSync('./snapshots/' + moment().format("MMM_do_YY_hh_mm_ss") + '.json', JSON.stringify(songs, null, 2), 'utf-8');
-
-      songsToMove.forEach(function(songToMove) {
-        console.log("Moving: " + (songToMove.track ? songToMove.track.title : "Non-Google Music song"));
-        gmusic.movePlayListEntryBefore(songToMove, firstSong.clientId, function(err, result){
-          if (err) console.log("ERROR:" + err);
-          console.log(result);
+        songsToMove.forEach(function(songToMove) {
+          console.log("Moving: " + (songToMove.track ? songToMove.track.title : "Non-Google Music song"));
+          gmusic.movePlayListEntryBefore(songToMove, firstSong.clientId, function(err, result){
+            if (err) console.log("ERROR:" + err);
+            console.log(result);
+          });
         });
-      });
-  });
+    });
 
-});
+  });
+}, 15 * 1000);
